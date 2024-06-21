5848a24c4b3cfb29bbda44e50374fa736b371b8b
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-06-21 12:17:05 -0700

    fix

--- negative_binomial_evals.py
+++ negative_binomial_evals.py
@@ -8,48 +8,53 @@ from scipy.stats import nbinom
 from numba import njit, prange
 from profile_context import ProfileContext
 
-@njit(cache=True)
-def factorial_scalar(nn):
-    result = 1
+@njit(cache=True, parallel=False)
+def factorial_scalar(nn, log=False):
+    result = 0 if log else 1
 
     if nn <= 1:
         return result
 
-    for ii in prange(2, nn + 1):
-        result *= ii
+    if log:
+        for ii in prange(2, nn + 1):
+            result += np.log(ii)
+        
+    else:
+        for ii in prange(2, nn + 1):
+            result *= ii
 
     return result
 
 @njit(cache=True, parallel=False)
-def factorial_core(unique_fac, unique_nn):
+def factorial_core(unique_fac, unique_nn, log=False):
     for ii, un in enumerate(unique_nn):
-        unique_fac[ii] = factorial_scalar(un)
+        unique_fac[ii] = factorial_scalar(un, log=log)
 
     return unique_fac
 
 # NB no njit as return_inverse kwarg.
-def factorial(nn):
+def factorial(nn, log=False):
     result = np.ones_like(nn)
     unique_nn, idx = np.unique(nn, return_inverse=True)
     unique_fac = np.zeros_like(unique_nn)
-    unique_fac = factorial_core(unique_fac, unique_nn)
+    unique_fac = factorial_core(unique_fac, unique_nn, log=log)
     return unique_fac[idx]
 
-def stirlings(arg, max_arg=100_000):
+def stirlings(arg, max_arg=None):
     """
     return approx. to log(n!)
     """
     arg = np.atleast_1d(arg)
     result = np.zeros_like(arg)
 
-    isin = arg > max_arg
-
-    # TODO HACK
-    isin[:] = False
+    if max_arg is not None:
+        isin = (arg > max_arg)
+    else:
+        isin = np.zeros_like(result, dtype=bool) 
     
     result[isin] = arg[isin] * np.log(arg[isin]) - arg[isin]  # + O(ln arg)
-    result[~isin] = np.log(factorial(arg[~isin]))
-
+    result[~isin] = factorial(arg[~isin], log=True)
+    
     return result
 
 @njit(cache=True, parallel=False)
@@ -70,7 +75,7 @@ def nu_log_sum_core_vec(result, yis, inv_alphas):
 
 def nu_log_sum(yis, inv_alphas):
     yis = np.atleast_1d(yis)
-    result = np.zeros_like(yis)
+    result = np.zeros_like(yis, dtype=float)
 
     if isinstance(inv_alphas, (int, float)):
         inv_alphas = inv_alphas * np.ones_like(yis)
@@ -86,16 +91,19 @@ def log_like_v1(kk, nn, pp):
 
 def log_like_v2(kk, nn, pp):
     result = nn * np.log(pp) + kk * np.log(1.0 - pp)
+    result -= stirlings(kk)
+    
     result += np.log(gamma(kk + nn))
     result -= np.log(gamma(nn))
-    result -= stirlings(kk)
 
     return result
 
 def log_like_v3(kk, nn, pp):
     result = nn * np.log(pp) + kk * np.log(1.0 - pp)
     result -= stirlings(kk)
+    
     result += nu_log_sum(kk, nn)
+    
     return result
 
 def log_like(kk, nn, pp, version="v1"):
@@ -107,15 +115,17 @@ def log_like(kk, nn, pp, version="v1"):
         return log_like_v3(kk, nn, pp)
 
 if __name__ == "__main__":
-    nrepeat, version = 1000, "v3"
+    nrepeat, version = 1_000, "v1"
 
     # kk, nn, pp = 20, 30, 0.25
     kk, nn, pp = 8, 10, 0.41118372
     # kk, nn, pp = np.array([kk, kk]), np.array([nn, nn]), np.array([pp, pp])
 
-    # exp = np.log(gamma(kk + nn) / gamma(nn))
-    # result = nu_log_sum_core(kk, nn)
+    exp = np.log(gamma(kk + nn) / gamma(nn))
+    result = nu_log_sum(kk, nn)
 
+    # print(exp, result[0])
+    
     with ProfileContext() as context:
         for ii in range(nrepeat):
             result = log_like(kk, nn, pp, version=version)
