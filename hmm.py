230588a87e9cbd156233d45eb4561a344864a832
Michael J. Wilson <michaeljwilson@princeton.edu>
2024-11-15 12:08:35 -0500

    black

--- hmm.py
+++ hmm.py
@@ -9,12 +9,14 @@ torch.manual_seed(42)
 np.random.seed(42)
 random.seed(42)
 
+
 def get_device():
     return (
         "cuda"
         if torch.cuda.is_available()
         else "mps" if torch.backends.mps.is_available() else "cpu"
     )
+
 
 class Model(nn.Module):
     def __init__(self, nstates, nsteps=10):
@@ -32,18 +34,18 @@ class Model(nn.Module):
     def forward(self, start):
         raw = self.layers(start)
         return raw / torch.sum(raw)
-        
+
 
 if __name__ == "__main__":
     device = get_device()
     nstates, nsteps = 7, 100
-    
+
     model = Model(nstates=nstates, nsteps=nsteps).to(device)
 
     # NB we must start in a single state.
     start = torch.ones(nstates, dtype=torch.float32, device=device)
     start = start / torch.sum(start)
-    
+
     current = model(start)
 
     print(start)
@@ -51,11 +53,10 @@ if __name__ == "__main__":
 
     transfer = model.linear_layer.weight.data.cpu().numpy()
     nstep_transfer = np.linalg.matrix_power(transfer, nsteps)
-    
+
     result = np.dot(nstep_transfer, start.cpu().numpy())
     result = result / np.sum(result)
 
     print(result)
-    
-    assert np.allclose(current.cpu().detach().numpy(), result)
 
+    assert np.allclose(current.cpu().detach().numpy(), result)
